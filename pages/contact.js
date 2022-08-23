58a8673928c2b911a6ac7fbef29e67f55f47797f
K1ngQ1 <wigginsquinten@gmail.com>
2022-08-23 11:36:52 +0200

    make it better

--- pages/contact.js
+++ pages/contact.js
@@ -5,13 +5,15 @@ import Layout from "../components/Layout";
 import Button from "react-bootstrap/Button";
 
 export default () => (
-  <Layout>
-    <div>
-      <h1>Contact Page</h1>
-      <h3>Email:</h3>
-      <p>wigginsquinten@gmail.com</p>
-      <h3>LinkedIn:</h3>
-      <Button>My LinkedIn</Button>
-    </div>
-  </Layout>
+    <Layout>
+        <div>
+            <h1>Contact Page</h1>
+            <h3>Email:</h3>
+            <p>wigginsquinten@gmail.com</p>
+            <h3>LinkedIn:</h3>
+            <a href="https://www.linkedin.com/in/quinten-wiggins-83a994229/">
+                <Button>My LinkedIn</Button>
+            </a>
+        </div>
+    </Layout>
 );
